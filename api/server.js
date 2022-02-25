ade06e07cdba8677d3c098091bee5170fcb4e156
Anastasia Lapteva <anastasia_laptev@hotmail.com>
2022-02-24 17:28:58 -0800

    added error handling

--- api/server.js
+++ api/server.js
@@ -7,6 +7,13 @@ const restrict = require('./middleware/restricted.js');
 const authRouter = require('./auth/auth-router.js');
 const jokesRouter = require('./jokes/jokes-router.js');
 
+const jsonErrorHandler = async (err, req, res, next) =>
+{
+    res.header("Content-Type", 'application/json');
+
+    res.status(err.status).send(JSON.stringify(err));
+};
+
 const server = express();
 
 server.use(helmet());
@@ -15,5 +22,6 @@ server.use(express.json());
 
 server.use('/api/auth', authRouter);
 server.use('/api/jokes', restrict, jokesRouter); // only logged-in users should have access!
+server.use(jsonErrorHandler);
 
 module.exports = server;
